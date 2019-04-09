1529a31e03e5addc80404603a5a6d70f2cb39f65
Fernando <fernando.pavanello2@gmail.com>
2019-04-09 09:05:51 +0000

    Implementando itens do menu

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { MenuItem } from "./menu-item.model";
+
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "mt-menu",
   templateUrl: "./menu.component.html"
 })
 export class MenuComponent implements OnInit {
-  constructor() {}
+  menu: Observable<MenuItem[]>;
 
-  ngOnInit() {}
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.menu = this.restaurantsService.menuOfRestaurant(
+      this.route.parent.snapshot.params["id"]
+    );
+  }
+
+  addMenuItem(item: MenuItem) {
+    console.log(item);
+  }
 }
