9548b1f5ad5ac41b836d7cfea23a560518254a73
Fernando <fernando.pavanello2@gmail.com>
2019-04-10 08:29:38 +0000

    Criando componente de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
 
 import { RestaurantsService } from "./restaurants/restaurants.service";
 import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-
     MenuComponent,
     ShoppingCartComponent,
     MenuItemComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent
   ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [
