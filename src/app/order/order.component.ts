4b618ad29cc2d7f07eb5e8d9d452890f396d1168
Fernando <fernando.pavanello2@gmail.com>
2019-04-17 09:04:43 +0000

    Criada tela de sucesso, feita navageção via router

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Router } from "@angular/router";
+
 import { RadioOption } from "app/shared/radio/radio-option.model";
 import { OrderService } from "./order.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
@@ -18,7 +20,7 @@ export class OrderComponent implements OnInit {
     { label: "Cartão Refeição", value: "REF" }
   ];
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService, private router: Router) {}
 
   ngOnInit() {}
 
@@ -48,6 +50,7 @@ export class OrderComponent implements OnInit {
     );
 
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      this.router.navigate(["/order-summary"]);
       this.orderService.clear();
     });
   }
